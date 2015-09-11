ef145df82d4944940e728f90d718547828fef05d
Adam <afranklint16@gmail.com>
2015-09-11 11:56:32 -0700

    Fix plugin metrics
    Replace Metrics.java with MetricsLite.java and fixed and reformatted
    SurvivalGames.java

--- src/main/java/org/mcsg/survivalgames/SurvivalGames.java
+++ src/main/java/org/mcsg/survivalgames/SurvivalGames.java
@@ -14,13 +14,25 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.mcsg.survivalgames.events.*;
+import org.mcsg.survivalgames.events.BandageUse;
+import org.mcsg.survivalgames.events.BreakEvent;
+import org.mcsg.survivalgames.events.ChestReplaceEvent;
+import org.mcsg.survivalgames.events.CommandCatch;
+import org.mcsg.survivalgames.events.DeathEvent;
+import org.mcsg.survivalgames.events.JoinEvent;
+import org.mcsg.survivalgames.events.KeepLobbyLoadedEvent;
+import org.mcsg.survivalgames.events.KitEvents;
+import org.mcsg.survivalgames.events.LogoutEvent;
+import org.mcsg.survivalgames.events.MoveEvent;
+import org.mcsg.survivalgames.events.PlaceEvent;
+import org.mcsg.survivalgames.events.SignClickEvent;
+import org.mcsg.survivalgames.events.SpectatorEvents;
+import org.mcsg.survivalgames.events.TeleportEvent;
 import org.mcsg.survivalgames.hooks.HookManager;
 import org.mcsg.survivalgames.logging.LoggingManager;
 import org.mcsg.survivalgames.logging.QueueManager;
 import org.mcsg.survivalgames.stats.StatsManager;
 import org.mcsg.survivalgames.util.ChestRatioStorage;
-import org.mcsg.survivalgames.util.ChestRatioStorageOLD;
 import org.mcsg.survivalgames.util.DatabaseManager;
 
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
@@ -33,23 +45,52 @@ public class SurvivalGames extends JavaPlugin {
 	public static boolean config_todate = false;
 	public static int config_version = 3;
 
-	public static List < String > auth = Arrays.asList(new String[] {
-			"Double0negative", "iMalo", "Medic0987", "alex_markey", "skitscape", "AntVenom", "YoshiGenius", "pimpinpsp", "WinryR", "Jazed2011",
-			"KiwiPantz", "blackracoon", "CuppingCakes", "4rr0ws", "Fawdz", "Timothy13", "rich91", "ModernPrestige", "Snowpool", "egoshk", 
-			"nickm140",  "chaseoes", "Oceangrass", "GrailMore", "iAngelic", "Lexonia", "ChaskyT", "Anon232", "IngeniousGamer" //:) -Bryce
+	public static List<String> auth = Arrays.asList(new String[] {
+			"Double0negative",
+			"iMalo",
+			"Medic0987",
+			"alex_markey",
+			"skitscape",
+			"AntVenom",
+			"YoshiGenius",
+			"pimpinpsp",
+			"WinryR",
+			"Jazed2011",
+			"KiwiPantz",
+			"blackracoon",
+			"CuppingCakes",
+			"4rr0ws",
+			"Fawdz",
+			"Timothy13",
+			"rich91",
+			"ModernPrestige",
+			"Snowpool",
+			"egoshk",
+			"nickm140",
+			"chaseoes",
+			"Oceangrass",
+			"GrailMore",
+			"iAngelic",
+			"Lexonia",
+			"ChaskyT",
+			"Anon232",
+			"IngeniousGamer" // :) -Bryce
 	});
 
 	SurvivalGames p = this;
+
+	@Override
 	public void onDisable() {
 		disabling = false;
 		PluginDescriptionFile pdfFile = p.getDescription();
 		SettingsManager.getInstance().saveSpawns();
 		SettingsManager.getInstance().saveSystemConfig();
-		for (Game g: GameManager.getInstance().getGames()) {
-			try{
+		for (Game g : GameManager.getInstance().getGames()) {
+			try {
 				g.disable();
-			}catch(Exception e){
-				//will throw useless "tried to register task blah blah error." Use the method below to reset the arena without a task.
+			} catch (Exception e) {
+				// will throw useless "tried to register task blah blah error."
+				// Use the method below to reset the arena without a task.
 			}
 			QueueManager.getInstance().rollback(g.getID(), true);
 		}
@@ -57,22 +98,24 @@ public class SurvivalGames extends JavaPlugin {
 		logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " has now been disabled and reset");
 	}
 
+	@Override
 	public void onEnable() {
 		logger = p.getLogger();
 
-		//ensure that all worlds are loaded. Fixes some issues with Multiverse loading after this plugin had started
+		// ensure that all worlds are loaded. Fixes some issues with Multiverse
+		// loading after this plugin had started
 		getServer().getScheduler().scheduleSyncDelayedTask(this, new Startup(), 10);
 		try {
-			new Metrics(this).start();
+			new MetricsLite(this).start();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-
 	}
 
 	class Startup implements Runnable {
+		@Override
 		public void run() {
 			datafolder = p.getDataFolder();
 
@@ -83,9 +126,10 @@ public class SurvivalGames extends JavaPlugin {
 			MessageManager.getInstance().setup();
 			GameManager.getInstance().setup(p);
 
-			try { // try loading everything that uses SQL. 
+			try { // try loading everything that uses SQL.
 				FileConfiguration c = SettingsManager.getInstance().getConfig();
-				if (c.getBoolean("stats.enabled")) DatabaseManager.getInstance().setup(p);
+				if (c.getBoolean("stats.enabled"))
+					DatabaseManager.getInstance().setup(p);
 				QueueManager.getInstance().setup();
 				StatsManager.getInstance().setup(p, c.getBoolean("stats.enabled"));
 				dbcon = true;
@@ -116,23 +160,19 @@ public class SurvivalGames extends JavaPlugin {
 			pm.registerEvents(new KitEvents(), p);
 			pm.registerEvents(new KeepLobbyLoadedEvent(), p);
 
-
-			for (Player p: Bukkit.getOnlinePlayers()) {
+			for (Player p : Bukkit.getOnlinePlayers()) {
 				if (GameManager.getInstance().getBlockGameId(p.getLocation()) != -1) {
 					p.teleport(SettingsManager.getInstance().getLobbySpawn());
 				}
 			}
 
-			//   new Webserver().start();
+			// new Webserver().start();
 		}
 	}
 
 	public void setCommands() {
 		getCommand("survivalgames").setExecutor(new CommandHandler(p));
 	}
-
-
-
 
 	public static File getPluginDataFolder() {
 		return datafolder;
@@ -151,21 +191,21 @@ public class SurvivalGames extends JavaPlugin {
 		}
 	}
 
-	public static void $(String msg){
+	public static void $(String msg) {
 		logger.log(Level.INFO, msg);
 	}
 
-	public static void $(Level l, String msg){
+	public static void $(Level l, String msg) {
 		logger.log(l, msg);
 	}
 
-	public static void debug(String msg){
-		if(SettingsManager.getInstance().getConfig().getBoolean("debug", false))
-			$("[Debug] "+msg);
+	public static void debug(String msg) {
+		if (SettingsManager.getInstance().getConfig().getBoolean("debug", false))
+			$("[Debug] " + msg);
 	}
 
 	public static void debug(int a) {
-		if(SettingsManager.getInstance().getConfig().getBoolean("debug", false))
-			debug(a+"");
+		if (SettingsManager.getInstance().getConfig().getBoolean("debug", false))
+			debug(a + "");
 	}
 }
